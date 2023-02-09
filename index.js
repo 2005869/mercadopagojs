4dc15b0e61a4190e4b8e9a0de03d92d8a7629549
2005869 <2005869@aluno.univesp.br>
2023-02-09 19:14:37 -0300

    new route

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ MercadoPago.configure({
     access_token: secrets.Access_Token
 });
 
+
+
 //Rotes
 app.get('/', (req, res) => {
     res.send('hello');
@@ -25,7 +27,7 @@ app.get('/payment', async (req, res) => {
                 title: 'smartphone 5g',
                 quantity: 1,
                 currency_id: 'BRL',
-                unit_price: parseFloat(20.99)
+                unit_price: parseFloat(1220.99)
             }
         ],
         payer: {
@@ -34,16 +36,23 @@ app.get('/payment', async (req, res) => {
         external_reference: id
     }
 
+    console.log('ID ' + id);
+
     try{
         const payment = await MercadoPago.preferences.create(data);
         return res.redirect(payment.body.init_point);
     }catch(err){
         console.log(err);
-        return res.send(err.message);
+        return res.send(err.status_detail);
     }
     
 });
 
+
+app.post('/not', (req, res) => {
+    console.log(req.query);
+    res.send('ok');
+});
 
 app.listen(SERVERPORT, () => {
     console.log('server run');
