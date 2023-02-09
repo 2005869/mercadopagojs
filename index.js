455984476fe1f8afda00bec8289464e020ded390
2005869 <2005869@aluno.univesp.br>
2023-02-08 22:31:54 -0300

    simulate a payment

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ app.get('/', (req, res) => {
     res.send('hello');
 });
 
+app.get('/payment', async (req, res) => {
+    var id = '' + Date.now();
+    var email = 'client@client.com';
+    const data = {
+        items: [
+            item = {
+                id,
+                title: 'smartphone 5g',
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(20.99)
+            }
+        ],
+        payer: {
+            email
+        },
+        external_reference: id
+    }
+
+    try{
+        const payment = await MercadoPago.preferences.create(data);
+        return res.redirect(payment.body.init_point);
+    }catch(err){
+        console.log(err);
+        return res.send(err.message);
+    }
+    
+});
+
 
 app.listen(SERVERPORT, () => {
     console.log('server run');
